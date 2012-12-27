42c5e410c0a8be0e42decf8c48e8f7b854c595f4
Houssem NJAH <njah.houssem@exoplatform.com>
2012-12-27 17:51:06 +0100

    CECP-60 : create category
    Signed-off-by: Houssem NJAH <njah.houssem@exoplatform.com>

--- api/ecp-api-service/src/main/java/org/exoplatform/ecp/services/ParameterizedListService.java
+++ api/ecp-api-service/src/main/java/org/exoplatform/ecp/services/ParameterizedListService.java
@@ -7,17 +7,69 @@ import org.exoplatform.container.xml.InitParams;
 public class ParameterizedListService {
 	
 	private List<String> companyTypes;
+	private List<String> categoryProperties;
+	private List<String> product;
+	private List<String> family;
+	private List<String> sla;
+	private List<String> core;
+	private List<String> duration;
+	private List<String> partnership;
+	private List<String> training;
+	private List<String> location;
 	
+	@SuppressWarnings("unchecked")
 	public ParameterizedListService(InitParams params){
 		companyTypes = params.getValuesParam("companyTypes").getValues();
+		categoryProperties = params.getValuesParam("category.properties").getValues();
+		product = params.getValuesParam("product").getValues();
+		family = params.getValuesParam("family").getValues();
+		sla = params.getValuesParam("sla").getValues();
+		core = params.getValuesParam("core").getValues();
+		duration = params.getValuesParam("duration").getValues();
+		partnership = params.getValuesParam("partnership").getValues();
+		training = params.getValuesParam("training").getValues();
+		location = params.getValuesParam("location").getValues();
+		
 	}
 
 	public List<String> getCompanyTypes() {
 		return companyTypes;
 	}
 
-	public void setCompanyTypes(List<String> companyTypes) {
-		this.companyTypes = companyTypes;
+	public List<String> getCategoryProperties() {
+		return categoryProperties;
 	}
-	
+
+	public List<String> getProduct() {
+		return product;
+	}
+
+	public List<String> getFamily() {
+		return family;
+	}
+
+	public List<String> getSla() {
+		return sla;
+	}
+
+	public List<String> getCore() {
+		return core;
+	}
+
+	public List<String> getDuration() {
+		return duration;
+	}
+
+	public List<String> getPartnership() {
+		return partnership;
+	}
+
+	public List<String> getTraining() {
+		return training;
+	}
+
+	public List<String> getLocation() {
+		return location;
+	}
+
 }
