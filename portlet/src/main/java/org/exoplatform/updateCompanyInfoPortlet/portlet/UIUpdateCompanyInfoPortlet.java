d709d61939c03110974f2c5390822c7816c8d060
ayoub zayati <ayoub.ezzayati@exoplatform.com>
2013-01-02 17:47:15 +0100

    CECP-54: 4.2.4.2 Update Company Info Add search form
    Signed-off-by: ayoub zayati <ayoub.ezzayati@exoplatform.com>

--- portlet/src/main/java/org/exoplatform/updateCompanyInfoPortlet/portlet/UIUpdateCompanyInfoPortlet.java
+++ portlet/src/main/java/org/exoplatform/updateCompanyInfoPortlet/portlet/UIUpdateCompanyInfoPortlet.java
@@ -10,17 +10,24 @@ import javax.jcr.query.QueryManager;
 
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIGrid;
+import org.exoplatform.webui.core.UIPageIterator;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
 import org.exoplatform.webui.core.model.SelectItemOption;
+import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.form.UIFormInputSet;
 import org.exoplatform.webui.form.UIFormSelectBox;
 import org.exoplatform.webui.form.UIFormStringInput;
 
-@ComponentConfig(lifecycle = UIApplicationLifecycle.class, template = "/groovy/ecp-portlets/portlet/updateCompanyInfoPortlet/UIUpdateCompanyInfoPortlet.gtmpl")
+@ComponentConfig(lifecycle = UIApplicationLifecycle.class, template = "/groovy/ecp-portlets/portlet/updateCompanyInfoPortlet/UIUpdateCompanyInfoPortlet.gtmpl", events = @EventConfig(listeners = UIUpdateCompanyInfoPortlet.EditCompanyActionListener.class))
 public class UIUpdateCompanyInfoPortlet extends UIPortletApplication {
 
 	public static final String COMPANY_NAME = "name";
@@ -32,6 +39,8 @@ public class UIUpdateCompanyInfoPortlet extends UIPortletApplication {
 	private static final String[] COMPANY_BEAN_FIELD = {COMPANY_NAME, COUNTRY, TYPE};
 
 	private static final String[] COMPANY_ACTION = {"EditCompany"};
+	
+	UIGrid grid_;
 
 	@SuppressWarnings("unchecked")
 	private final static List<SelectItemOption<String>> OPTIONS_ = Collections.unmodifiableList(Arrays.asList(new SelectItemOption<String>(COMPANY_NAME, COMPANY_NAME), new SelectItemOption<String>(COUNTRY, COUNTRY), new SelectItemOption<String>(TYPE, TYPE)));
@@ -39,14 +48,14 @@ public class UIUpdateCompanyInfoPortlet extends UIPortletApplication {
 	public UIUpdateCompanyInfoPortlet() throws Exception {
 		UISearchForm uiForm = addChild(UISearchForm.class, null, null);
 	    uiForm.setOptions(OPTIONS_);
-	    UIGrid grid_ = addChild(UIGrid.class, null, "UIListUsersGird");
+	    grid_ = addChild(UIGrid.class, null, "UIListCompaniesGrid");
 	    grid_.configure(COMPANY_NAME, COMPANY_BEAN_FIELD, COMPANY_ACTION);
-	    grid_.getUIPageIterator().setId("UIListUsersIterator");
+	    grid_.getUIPageIterator().setId("UIListCompaniesIterator");
 	    grid_.getUIPageIterator().setParent(this);
 	}
 	
 	public void quickSearch(UIFormInputSet quickSearchInput) throws Exception {
-	    UIFormStringInput input = (UIFormStringInput)quickSearchInput.getChild(0);
+		UIFormStringInput input = (UIFormStringInput)quickSearchInput.getChild(0);
 	    UIFormSelectBox select = (UIFormSelectBox)quickSearchInput.getChild(1);
 		ExoContainer exoContainer = ExoContainerContext.getCurrentContainer();
         RepositoryService rs = (RepositoryService) exoContainer.getComponentInstanceOfType(RepositoryService.class);
@@ -54,39 +63,28 @@ public class UIUpdateCompanyInfoPortlet extends UIPortletApplication {
         QueryManager queryManager = null;
         queryManager = session.getWorkspace().getQueryManager();
         StringBuilder queryString = new StringBuilder("select * from ecp:company where jcr:path like '/companies/%'");
-        if (input.getValue() != "") {
-        	queryString.append (" and " + "ecp:" + select.getLabel() + " like '" + input.getValue() + "%'");
+        if (input.getValue() != null) {
+        	queryString.append (" and " + "ecp:" + select.getValue() + " like '" + input.getValue() + "%'");
         }
 		Query query = queryManager.createQuery(queryString.toString(), Query.SQL);
-			
-	      //Query query = new Query();
-
-	      
-//	      String name = input.getValue();
-//	      if (name == null || name.equals(""))
-//	      {
-//	         search(new Query());
-//	         return;
-//	      }
-//	      if (name.indexOf("*") < 0)
-//	      {
-//	         if (name.charAt(0) != '*')
-//	            name = "*" + name;
-//	         if (name.charAt(name.length() - 1) != '*')
-//	            name += "*";
-//	      }
-//	      name = name.replace('?', '_');
-//	      String selectBoxValue = select.getValue();
-//	      if (selectBoxValue.equals(USER_NAME))
-//	         query.setUserName(name);
-//	      if (selectBoxValue.equals(LAST_NAME))
-//	         query.setLastName(name);
-//	      if (selectBoxValue.equals(FIRST_NAME))
-//	         query.setFirstName(name);
-//	      if (selectBoxValue.equals(EMAIL))
-//	         query.setEmail(name);
-//	      search(query);
-		System.out.println("buggggggggggggggggla");
+	    search(query);
+	}
+	
+	private void search(Query query) throws Exception {
+		grid_.getUIPageIterator().setPageList(new CompanyList(query, 10));
+	    UIPageIterator pageIterator = grid_.getUIPageIterator();
+	    if (pageIterator.getAvailable() == 0) {
+	    	UIApplication uiApp = Util.getPortalRequestContext().getUIApplication();
+	        uiApp.addMessage(new ApplicationMessage("UISearchForm.msg.empty", null));
+	    }
+	}
+	
+	static public class EditCompanyActionListener extends EventListener<UIUpdateCompanyInfoPortlet>
+	   {
+	      public void execute(Event<UIUpdateCompanyInfoPortlet> event) throws Exception
+	      {
+	    	  String companyname = event.getRequestContext().getRequestParameter(OBJECTID);
+	    	  System.out.println("Company to be edited: " + companyname);
+	      }
 	   }
-
 }
